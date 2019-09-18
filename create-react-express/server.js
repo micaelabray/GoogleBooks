f5e05d66e2aef8e03202f808ff068973c2472217
Micaela Bray <micaela.bray@gmail.com>
2019-09-17 19:08:34 -0500

    updates App.js files and adds in Navbar component

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+const db = require("./models");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,10 +15,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
-// Send every other request to the React app
-// Define any API routes before this runs
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
